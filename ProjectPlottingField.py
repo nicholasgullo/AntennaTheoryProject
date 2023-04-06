2adc3ed6fc1e3c08b4ac45ad7e7eadb3c29a0b82
nicholasgullo <70230653+nicholasgullo@users.noreply.github.com>
2023-04-05 22:12:30 -0700

    Update to Problem1

--- ProjectPlottingField.py
+++ ProjectPlottingField.py
@@ -1,31 +1,45 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-N = 20 # Number of elements in the array
-d = 0.5 # Element spacing in wavelengths
-theta = np.linspace(-np.pi/2, np.pi/2, 1000) # Theta values for plotting
+# Define the number of elements in the array
+N = 12
 
-# Set up the array weights with arbitrary amplitude and phase
-w = np.random.rand(N) * np.exp(1j*np.random.rand(N)*2*np.pi)
+# Define the spacing between the elements
+d = 0.5
 
-# Compute the array factor using complex exponentials
-af = np.zeros_like(theta, dtype=np.complex64)
-for i in range(N):
-    af += w[i] * np.exp(1j*2*np.pi*i*d*np.sin(theta))
+# Define the wavenumber
+k = 2*np.pi
 
-# Compute the normalized array pattern in dB
-ap = 20*np.log10(np.abs(af)/np.max(np.abs(af)))
 
-# Plot the normalized array pattern in dB vs. theta
-plt.plot(theta, ap)
-plt.xlabel('Angle (radians)')
-plt.ylabel('Normalized Array Pattern (dB)')
-plt.title('Linear Array Pattern')
-plt.grid(True)
+# Define the array factor
+theta = np.linspace(-np.pi/2, np.pi/2, 1000)
+AF = np.zeros(theta.shape, dtype=np.complex128)
 
-# Compute the directivity and peak sidelobe level
-D = 4*np.pi/np.sum(np.abs(af)**2)
-psl = np.max(ap) - np.max(ap[np.abs(theta) > np.pi/N])
+for n in range(N):
+    AF += 1*np.exp(1j*k*n*d*np.cos(theta))
 
-print('Directivity: {:.2f}'.format(D))
-print('Peak Sidelobe Level: {:.2f} dB'.format(psl))
+# Plot the normalized pattern in dB vs. θ
+norm = np.max(20*np.log10(np.abs(AF)))
+plt.plot(np.rad2deg(theta), 20*np.log10(np.abs(AF))-norm)
+plt.xlabel('Angle (degrees)')
+plt.ylabel('Normalized magnitude (dB)')
+plt.title('Array pattern')
+plt.ylim(-30,0)
+plt.grid()
+
+
+# Compute the directivity (from direct pattern integration)
+phi = np.linspace(0, 2*np.pi, 361)
+P = np.zeros(phi.shape, dtype=np.float64)
+
+for p in range(phi.shape[0]):
+    P[p] = np.sum(np.abs(AF*np.exp(-1j*k*d*np.cos(theta)*np.sin(phi[p]))))**2
+    
+directivity = 4*np.pi*np.max(P)/np.sum(P)
+print('Directivity =', directivity)
+
+# Compute the peak sidelobe level (first sidelobe) in dB
+psl = np.max(20*np.log10(np.abs(AF)))-np.max(20*np.log10(np.abs(AF[theta>0])))
+print('Peak sidelobe level =', psl, 'dB')
+
+plt.show()
