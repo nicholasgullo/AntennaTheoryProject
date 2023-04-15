2eaef6517e16a15cd1f0f2af92f83a80f10b70c7
nicholasgullo <70230653+nicholasgullo@users.noreply.github.com>
2023-04-15 14:40:24 -0700

    Update

--- ProjectPlottingField.py
+++ ProjectPlottingField.py
@@ -10,15 +10,23 @@ d = 0.5
 # Define the wavenumber
 k = 2*np.pi
 
+# Beta value for array factor
+B = 0 # Boadside array
+#B = np.pi/2 # Standard ULA (Main beam at 0 degrees)
 
 # Define the array factor
+
 theta = np.linspace(-np.pi/2, np.pi/2, 1000)
+#theta = np.linspace(0, np.pi, 1000)
+
 AF = np.zeros(theta.shape, dtype=np.complex128)
 
-ArrayWeights= np.array([-5.790, -2.050, -0.485, -1.177, -0.161, -1.00, 1.00, 1.161, 1.177, 0.485, 2.050, 5.790])
+#ArrayWeights= np.array([-5.790, -2.050, 0.485, 1.177, 0.161, -1.00, 0, 1.00, -0.161, -1.177, -0.485, 2.050, 5.790])
+ArrayWeights= np.array([-1.00, 0.161, 1.177, 0.485, -2.050, -5.790,0, 5.790, 2.050, -0.485, -1.177, -0.161, 1.00])
 
-for n in range(N):
-    AF += ArrayWeights[n]*np.exp(1j*k*n*d*np.cos(theta))
+for n in [-6,-5,-4,-3,-2,-1,1,2,3,4,5,6]:
+    AF += ArrayWeights[n+6] * np.exp(1j*k*n*d*np.cos(theta + B) )
+    #AF += np.exp(1j*k*n*d*np.cos(theta + B) )
 
 #Plot the normalized pattern in dB vs. θ
 norm = np.max(20*np.log10(np.abs(AF)))
@@ -35,6 +43,7 @@ plt.grid()
 
 
 directivity = 2*N*(d)
+directivity = 10*np.log10(directivity)
 print('Directivity =', directivity)
 
 # Compute the peak sidelobe level (first sidelobe) in dB
